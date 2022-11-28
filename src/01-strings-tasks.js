7bfde5ea54a31765ad706213f5b63cd6d0411651
alexpodelinskii <nikitik007@yandex.ru>
2022-11-29 02:19:56 +0300

    feat : change crlf to lf

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -9,7 +9,7 @@
  * Returns the result of concatenation of two strings.
  *
  * @param {string} value1
- * @param {string} value2
+ *  *  * @param {string} value2
  * @return {string}
  *
  * @example
